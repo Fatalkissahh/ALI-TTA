f455ecdf39846d85a473f01d0bffbea62ca6a093
Fatalkissahh <101218501+Fatalkissahh@users.noreply.github.com>
2022-05-03 20:09:03 +0100

    Add files via upload

---
+++ HashMaps.java
@@ -0,0 +1,20 @@
+package com.ashaikh.learn.java;
+
+public class HashMaps {
+	// Import the HashMap class
+	import java.util.HashMap;
+
+	
+	  public static  main(String[] args) {
+	    // Create a HashMap object called capitalCities
+	    HashMap<String, String> capitalCities = new HashMap<String, String>();
+
+	    // Add keys and values (Country, City)
+	    capitalCities.put("England", "London");
+	    capitalCities.put("Germany", "Berlin");
+	    capitalCities.put("Norway", "Oslo");
+	    capitalCities.put("USA", "Washington DC");
+	    System.out.println(capitalCities);
+	  }
+	}
+}
